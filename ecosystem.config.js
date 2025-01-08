f516f2d16443c9f2082d08fed05498edf8d15354
ALLEN_PC\84423 <844235837@qq.com>
2025-01-08 22:31:45 +0800

    feat: 修改配置

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,10 +2,12 @@ module.exports = {
   apps: [
     {
       name: 'ai-batch-reader',
-      script: 'nodemon ./index.js',
+      script: './index.js',
       instances: 2,
       exec_mode: 'cluster',
       max_memory_restart: '1G',
+      watch: false,
+      ignore_watch: ['node_modules', 'logs', '*.log', '.git'],
       env: {
         NODE_ENV: 'production',
         PORT: 3015,
